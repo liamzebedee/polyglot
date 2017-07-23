2027c9fb513e6dea6b828e929176f1c6f68d7381
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2017-07-24 04:10:03 +1000

    3

--- polyglot.go
+++ polyglot.go
@@ -3,20 +3,60 @@ package main
 import (
     "os"
     "fmt"
+    "net"
 )
+
+
+
+func polyglot() {
+    polyglotSocket := "/tmp/polyglot.sock"
+    addr := &net.UnixAddr{polyglotSocket, "unix"}
+
+
+    needToStartManager := false
+    if _, err := os.Stat(polyglotSocket); os.IsNotExist(err) {
+        panic("Socket doesn't exist yet")
+        list, err := net.ListenUnix("unix", addr)
+
+        go func() {
+            conn, err := list.AcceptUnix()
+            if err != nil {
+                panic("Couldn't accept conn :"+err.Error())
+            }
+
+            go func() {
+                buf := make([]byte, 2048)
+                for {
+                    buf, err := conn.ReadFromUnix()
+                }
+            }
+        }
+
+        list.SetUnlinkOnClose(true)
+    }
+
+    conn, err := net.DialUnix("unix", nil, addr)
+    if err != nil {
+        panic("Error connecting to Polyglot controller")
+    }
+}
+
+
 
 func main() {
     fmt.Println("polyglot by @liamzebedee")
     fmt.Println("Support developer experience - liamz.co")
 
+
     if len(os.Args) < 2 {
         return
     }
-
     firstArg := os.Args[1]
-    if firstArg[0] == '-' {
+    if len(firstArg) > 0 && firstArg[0] == '-' {
         fmt.Println("It is a command")
     } else {
+        polyglot()
+
         fmt.Println("Let's build")
     }
 }
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+go build && ./polyglot what
